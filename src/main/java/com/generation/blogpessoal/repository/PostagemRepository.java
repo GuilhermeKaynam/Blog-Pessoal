59b554b811625e13d42e5e00b23d0b4050969d4d
Guilherme-Kaynam <guilhermekaynam2006@gmail.com>
2024-11-27 12:01:28 -0300

    Banco de dados

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.generation.blogpessoal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.generation.blogpessoal.model.Postagens;
-
-public interface PostagemRepository extends JpaRepository<Postagens, Long> {
-
-}
